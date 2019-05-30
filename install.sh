38f88eba024aa5033172b2375ee5e1e2990d2176
Alexia lechot <admin@uxmilk.co>
2019-05-30 15:28:05 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,7 @@
 # install fish + node
 brew tap dart-lang/dart
+brew tap buo/cask-upgrade
+
 brew install \
     yarn wget fish php php-cs-fixer php-code-sniffer \
     phpunit ctop golang dart composer screenfetch php \
@@ -17,24 +19,16 @@ curl https://git.io/fisher --create-dirs -sLo ~/.config/fish/functions/fisher.fi
 # reset terminal
 reset
 
-# add fisher package
-fisher add fishpkg/fish-prompt-metro
-fisher add franciscolourenco/done
-fisher
-fisher self-update
-
-# reset terminal
-reset
-
 # easy python
 sudo easy_install pip
 
 # install software with cask
-brew cask install gitkraken visual-studio-code arduino \
+brew cask install \
+gitkraken visual-studio-code visual-studio arduino \
 firefox google-chrome unity-hub handbrake onyx whatsapp  \
 iina Teamviewer webtorrent imageoptim sketch sketchpacks \
 appcleaner alfred java balenaetcher docker insomnia \ 
-mongodb-compass openinterminal motrix bitwarden
+mongodb-compass-community openinterminal motrix bitwarden
 
 # install global package node
 yarn global add tslint jshint eslint sass-lint webpack \
@@ -42,3 +36,18 @@ yarn global add tslint jshint eslint sass-lint webpack \
     create-react-app @angular/cli typescript \
     laravel-echo-server nodemon jscs prettier
 
+# install package php
+sudo pecl install xdebug
+
+# reset terminal
+reset
+
+# add fisher package
+fisher add fishpkg/fish-prompt-metro
+fisher add franciscolourenco/done
+fisher
+fisher self-update
+
+# reset
+curl -Lo ~/.config/fish/functions/humanize_duration.fish --create-dirs https://raw.githubusercontent.com/fishpkg/fish-humanize-duration/master/humanize_duration.fish
+curl -Lo ~/.config/fish/conf.d/done.fish --create-dirs https://raw.githubusercontent.com/franciscolourenco/done/master/conf.d/done.fish
