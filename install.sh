02626a45e4c7135d27aeb321ca5a87a2439e2063
Alexia Lechot <alexia@krakenwave.ch>
2022-09-06 20:42:08 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,18 +3,9 @@ brew tap dart-lang/dart
 brew tap buo/cask-upgrade
 
 brew install \
-    node yarn wget fish php php-cs-fixer php-code-sniffer \
+    node yarn wget php php-cs-fixer php-code-sniffer \
     phpunit ctop golang dart composer screenfetch php \
     terminal-notifier shfmt
-
-# add default config fish
-sudo echo /usr/local/bin/fish >>/etc/shells
-chsh -s /usr/local/bin/fish
-echo "/usr/local/bin/fish" | sudo tee -a /etc/shells
-chsh -s /usr/local/bin/fish
-
-# install fisher
-curl https://git.io/fisher --create-dirs -sLo ~/.config/fish/functions/fisher.fish
 
 # reset terminal
 reset
@@ -33,19 +24,3 @@ yarn global add tslint jshint eslint sass-lint webpack \
     webpack-cli surge pure-prompt @vue/cli \
     create-react-app @angular/cli typescript \
     laravel-echo-server nodemon jscs prettier
-
-# install package php
-sudo pecl install xdebug
-
-# reset terminal
-reset
-
-# add fisher package
-fisher add fishpkg/fish-prompt-metro
-fisher add franciscolourenco/done
-fisher
-fisher self-update
-
-# reset
-curl -Lo ~/.config/fish/functions/humanize_duration.fish --create-dirs https://raw.githubusercontent.com/fishpkg/fish-humanize-duration/master/humanize_duration.fish
-curl -Lo ~/.config/fish/conf.d/done.fish --create-dirs https://raw.githubusercontent.com/franciscolourenco/done/master/conf.d/done.fish
