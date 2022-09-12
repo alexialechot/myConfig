fd8c1eabeed427c66f3604d8a69adb3febaaacc8
Alexia Lechot <alexia@krakenwave.ch>
2022-09-12 14:00:35 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -15,7 +15,7 @@ sudo easy_install pip
 
 # install software with cask
 brew install --cask \
-gitkraken visual-studio-code \
+gitkraken visual-studio-code zoom \
 firefox google-chrome handbrake onyx whatsapp \
 iina imageoptim appcleaner alfred figma
 
